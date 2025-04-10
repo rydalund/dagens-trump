51e92f85983e90c5e38f347ac69069fc469be2c3
rydalund <54313809+rydalund@users.noreply.github.com>
2025-04-10 11:25:30 +0200

    Date position fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// Date
+const dateElement = document.getElementById('current-date');
+const today = new Date();
+
+const weekdayNames = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'];
+const monthNames = ['januari', 'februari', 'mars', 'april', 'maj', 'juni',
+                    'juli', 'augusti', 'september', 'oktober', 'november', 'december'];
+
+const formattedDate = `${weekdayNames[today.getDay()]} ${today.getDate()} ${monthNames[today.getMonth()]} ${today.getFullYear()}`;
+dateElement.textContent = formattedDate;
+
 // Instagram-slider
 const instaContainer = document.getElementById("instagramContainer");
 document.querySelector(".instagram-prev").addEventListener("click", () => {
